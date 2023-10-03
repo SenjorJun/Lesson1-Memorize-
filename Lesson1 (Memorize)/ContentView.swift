b20293f844a341d4caf44e489d70f592c30a703c
SenjorJun <Solovyov.sergey98@yandex.ru>
2023-10-03 17:57:03 +0300

    Initial Commit

---
+++ Lesson1 (Memorize)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lesson1 (Memorize)
+//
+//  Created by Сергей Соловьёв on 03.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lesson1 (Memorize)/Lesson1__Memorize_App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson1__Memorize_App.swift
+//  Lesson1 (Memorize)
+//
+//  Created by Сергей Соловьёв on 03.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Lesson1__Memorize_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
